9ff2c7f60e20f70a852a6fe4935a458f1b6b5ed7
miud <miud.poon@gmail.com>
2013-09-15 20:16:18 +0800

    First Version

---
+++ Prototype/Config.js
@@ -0,0 +1,6 @@
+
+var FullHeight = 140; // default: 20
+var BarWidth = 4.5; // default: 5
+var ShowCityName = false; // true or false
+var BarFillColor = "#aaa";
+var MapFillColor = "#add"; // map color
